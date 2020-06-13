a688a5e1c9710ae72c12248a6ccd83436f9458b3
Michael Miller <icy.arctic.fox@gmail.com>
2020-06-13 15:10:49 -0600

    Reduce code duplication

--- src/gloop/vertex_array.cr
+++ src/gloop/vertex_array.cr
@@ -170,19 +170,12 @@ module Gloop
 
       # Sets the format of this attribute on the vertex array object.
       def format=(format : FloatVertexAttributePointer)
-        type = LibGL::VertexAttribType.new(format.type.value)
-        normalized = bool_to_int(format.normalized?)
-        checked do
-          LibGL.vertex_array_attrib_format(@vao, @index, format.size, type, normalized, format.offset)
-        end
+        set_format(format, format.offset)
       end
 
       # Sets the format of this attribute on the vertex array object.
       def format=(format : IntVertexAttributePointer)
-        type = LibGL::VertexAttribType.new(format.type.value)
-        checked do
-          LibGL.vertex_array_attrib_i_format(@vao, @index, format.size, type, format.offset)
-        end
+        set_format(format, format.offset)
       end
     end
   end
