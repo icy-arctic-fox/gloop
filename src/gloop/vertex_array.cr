006b0b8001a88a457c4083ea6429d239d11d8986
Michael Miller <icy.arctic.fox@gmail.com>
2020-06-13 22:11:13 -0600

    Add size

--- src/gloop/vertex_array.cr
+++ src/gloop/vertex_array.cr
@@ -124,9 +124,19 @@ module Gloop
 
     # Provides an intermediate interface to access attributes attached to the vertex array.
     struct AttributeCollection
+      include ErrorHandling
+
       # Creates the collection.
       # The *vao* should be the OpenGL ID of the vertex array to proxy access to.
       protected def initialize(@vao : LibGL::UInt)
+      end
+
+      # Maximum number of allowed vertex attributes.
+      def size
+        checked do
+          LibGL.get_integer_v(LibGL::GetPName::MaxVertexAttribs, out result)
+          result
+        end
       end
 
       # Retrieves the attribute with the specified index.
