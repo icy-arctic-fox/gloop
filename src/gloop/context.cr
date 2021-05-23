887ba44e8e49918f717565dcc899a00741ba86af
Michael Miller <icy.arctic.fox@gmail.com>
2021-05-23 15:28:35 -0600

    Add error handling mix-in

--- src/gloop/context.cr
+++ src/gloop/context.cr
@@ -1,10 +1,12 @@
 require "opengl"
 require "./context/*"
+require "./error_handling"
 
 module Gloop
   # Information about the current OpenGL context.
   module Context
     extend self
+    extend ErrorHandling
 
     # Retrieves the major portion of the OpenGL's context version.
     # For instance, if the OpenGL version is 4.6, 4 is returned.
@@ -16,8 +18,10 @@ module Gloop
     #
     # Minimum required version: 2.0
     def major_version
-      LibGL.get_integer_v(LibGL::GetPName::MajorVersion, out value)
-      value
+      checked do
+        LibGL.get_integer_v(LibGL::GetPName::MajorVersion, out value)
+        value
+      end
     end
 
     # Retrieves the minor portion of the OpenGL's context version.
@@ -30,8 +34,10 @@ module Gloop
     #
     # Minimum required version: 2.0
     def minor_version
-      LibGL.get_integer_v(LibGL::GetPName::MinorVersion, out value)
-      value
+      checked do
+        LibGL.get_integer_v(LibGL::GetPName::MinorVersion, out value)
+        value
+      end
     end
 
     # Retrieves the profile of the current OpenGL context.
@@ -43,7 +49,10 @@ module Gloop
     #
     # Minimum required version: 3.2
     def profile
-      LibGL.get_integer_v(LibGL::GetPName::ContextProfileMask, out value)
+      value = checked do
+        LibGL.get_integer_v(LibGL::GetPName::ContextProfileMask, out value)
+        value
+      end
       Profile.from_value(value)
     end
   end
