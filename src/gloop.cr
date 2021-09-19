2cfe2d9951fd4bf7afa0a8815a658ded0cedc164
Michael Miller <icy.arctic.fox@gmail.com>
2021-09-19 10:29:12 -0600

    Add context parameters

--- src/gloop.cr
+++ src/gloop.cr
@@ -1,8 +1,13 @@
 require "opengl"
-require "./gloop/**"
 
 # Object oriented OpenGL library.
 module Gloop
   # Current version of the shard.
   VERSION = {{ `shards version "#{__DIR__}"`.stringify.chomp }}
+
+  # Annotation for indicating underlying OpenGL functions and version requirements.
+  annotation GLFunction
+  end
 end
+
+require "./gloop/**"
--- src/gloop/context.cr
+++ src/gloop/context.cr
@@ -1,4 +1,6 @@
+require "./context/*"
 require "./error_handling"
+require "./parameters"
 
 module Gloop
   # Provides information about a pre-existing OpenGL context
@@ -8,34 +10,92 @@ module Gloop
   # Another mechanism is needed to create an OpenGL context.
   # Some possibilities are GLFW, SFML, and SDL.
   struct Context
-    # Wraps calls to OpenGL functions.
-    # All method calls delegate to the OpenGL function loader given in `#initialize`.
-    # OpenGL function calls are wrapped with error checking if enabled (see `Context#gl`).
-    private struct Proxy
-      include ErrorHandling
+    include Parameters
 
-      # Creates the proxy.
-      # Function calls are delegated to *loader*.
-      def initialize(@loader : OpenGL::Loader)
-      end
+    # Retrieves the major version number of the OpenGL API supported by the current context.
+    # For instance, if the OpenGL version is 4.6, 4 is returned.
+    #
+    # - OpenGL function: `glGetIntegerv`
+    # - OpenGL enum: `GL_MAJOR_VERSION`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGetIntegerv", enum: "GL_MAJOR_VERSION", version: "2.0")]
+    parameter MajorVersion, major_version : Int32
 
-      # Delegates calls to the OpenGL function loader.
-      # Wraps calls with error checking, when enabled.
-      # An `unchecked` option can be provided to conditionally check for errors.
-      macro method_missing(call)
-        {% if (args = call.named_args) && (arg = args.find { |arg| arg.name == :unchecked.id }) %}
-          if {{arg.value}}
-            @loader.{{call.name}}!({{call.args.splat}})
-          else
-            checked { @loader.{{call.name}}({{call.args.splat}}) }
-          end
-        {% elsif flag?(:release) && !flag?(:error_checking) %}
-          @loader.{{call.name}}!({{call.args.splat}})
-        {% else %}
-          checked { @loader.{{call}} }
-        {% end %}
-      end
-    end
+    # Retrieves the minor version number of the OpenGL API supported by the current context.
+    # For instance, if the OpenGL version is 4.6, 6 is returned.
+    #
+    # - OpenGL function: `glGetIntegerv`
+    # - OpenGL enum: `GL_MINOR_VERSION`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGetIntegerv", enum: "GL_MINOR_VERSION", version: "2.0")]
+    parameter MinorVersion, minor_version : Int32
+
+    # Retrieves the kinds of contexts supported.
+    #
+    # - OpenGL function: `glGetIntegerv`
+    # - OpenGL enum: `GL_CONTEXT_PROFILE_MASK`
+    # - OpenGL version: 3.2
+    @[GLFunction("glGetIntegerv", enum: "GL_CONTEXT_PROFILE_MASK", version: "3.2")]
+    parameter ContextProfileMask, profile : Profile
+
+    # Retrieves flags that were set when the context was created (such as debugging functionality).
+    #
+    # - OpenGL function: `glGetIntegerv`
+    # - OpenGL enum: `GL_CONTEXT_FLAGS`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGetIntegerv", enum: "GL_CONTEXT_FLAGS", version: "2.0")]
+    parameter ContextFlags, flags : Flags
+
+    # Retrieves a string containing the name of the company responsible for this GL implementation.
+    # This name does not change from release to release.
+    #
+    # - OpenGL function: `glGetString`
+    # - OpenGL enum: `GL_VENDOR`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGetString", enum: "GL_VENDOR")]
+    parameter Vendor, vendor : String
+
+    # Retrieves a string containing the name of the renderer.
+    # This name is typically specific to a particular configuration of a hardware platform.
+    # It does not change from release to release.
+    #
+    # - OpenGL function: `glGetString`
+    # - OpenGL enum: `GL_RENDERER`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGetString", enum: "GL_RENDERER")]
+    parameter Renderer, renderer : String
+
+    # Retrieves a version or release number.
+    #
+    # This usually takes the form:
+    # ```text
+    # MAJOR.MINOR[.RELEASE][ VENDOR]
+    # ```
+    # Vendor-specific information may follow the version string.
+    # Its format dependes on the implementation,
+    # but a space always separates the version number and the vendor-specific information.
+    #
+    # - OpenGL function: `glGetString`
+    # - OpenGL enum: `GL_VERSION`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGetString", enum: "GL_VERSION")]
+    parameter Version, version : String
+
+    # Retrieves a version or release number for the shading language (GLSL).
+    #
+    # This usually takes the form:
+    # ```text
+    # MAJOR.MINOR[.RELEASE][ VENDOR]
+    # ```
+    # Vendor-specific information may follow the version string.
+    # Its format dependes on the implementation,
+    # but a space always separates the version number and the vendor-specific information.
+    #
+    # - OpenGL function: `glGetString`
+    # - OpenGL enum: `GL_SHADING_LANGUAGE_VERSION`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGetString", enum: "GL_SHADING_LANGUAGE_VERSION")]
+    parameter ShadingLanguageVersion, shading_language_version : String
 
     # Constructs a reference to an existing context with pre-loaded functions.
     # The *loader* should be setup with OpenGL function addresses.
@@ -72,6 +132,35 @@ module Gloop
     # Errors are not raised after invalid OpenGL function calls.
     def gl
       @proxy
+    end
+
+    # Wraps calls to OpenGL functions.
+    # All method calls delegate to the OpenGL function loader given in `#initialize`.
+    # OpenGL function calls are wrapped with error checking if enabled (see `Context#gl`).
+    private struct Proxy
+      include ErrorHandling
+
+      # Creates the proxy.
+      # Function calls are delegated to *loader*.
+      def initialize(@loader : OpenGL::Loader)
+      end
+
+      # Delegates calls to the OpenGL function loader.
+      # Wraps calls with error checking, when enabled.
+      # An `unchecked` option can be provided to conditionally check for errors.
+      macro method_missing(call)
+        {% if (args = call.named_args) && (arg = args.find { |arg| arg.name == :unchecked.id }) %}
+          if {{arg.value}}
+            @loader.{{call.name}}!({{call.args.splat}})
+          else
+            checked { @loader.{{call.name}}({{call.args.splat}}) }
+          end
+        {% elsif flag?(:release) && !flag?(:error_checking) %}
+          @loader.{{call.name}}!({{call.args.splat}})
+        {% else %}
+          checked { @loader.{{call}} }
+        {% end %}
+      end
     end
   end
 end
