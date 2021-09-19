38c1ccd040bd38e591c81309fbeecc6210129819
Michael Miller <icy.arctic.fox@gmail.com>
2021-09-19 11:15:27 -0600

    Support custom OpenGL enums for parameters

--- src/gloop/parameters.cr
+++ src/gloop/parameters.cr
@@ -5,6 +5,8 @@ module Gloop
     # Defines a getter method that retrieves an OpenGL parameter.
     # The *pname* is the name of the OpenGL parameter to retrieve.
     # This should be an enum value (just the name) from `LibGL::GetPName`.
+    # If the enum is not in `LibGL::GetPName` (for instance, a constant under LibGL),
+    # specify the full path as such: `LibGL::NUM_SHADING_LANGUAGE_VERSIONS`.
     # The *name* will be the name of the generated method.
     # It can have a type annotation, which will control how the parameter's value is retrieved.
     # The parameter's value will be cast to the type.
@@ -30,44 +32,58 @@ module Gloop
       {% if name.is_a?(TypeDeclaration) %}
         {% type = name.type.resolve %}
         def {{name.var.id}} : {{type}}
+          %pname = {% if type <= String %}
+            {% if pname.id.includes? "::" %}
+              LibGL::StringName.new({{pname.id}}.to_u32!)
+            {% else %}
+              LibGL::StringName::{{pname.id}}
+            {% end %}
+          {% else %}
+            {% if pname.id.includes? "::" %}
+              LibGL::GetPName.new({{pname.id}}.to_u32!)
+            {% else %}
+              LibGL::GetPName::{{pname.id}}
+            {% end %}
+          {% end %}
+
           %return = begin
             {% if type < Enum %}
               %value = uninitialized Int32
-              gl.get_integer_v(LibGL::GetPName::{{pname.id}}, pointerof(%value))
+              gl.get_integer_v(%pname, pointerof(%value))
               {{type}}.from_value(%value)
 
             {% elsif type <= Int32 %}
               %value = uninitialized Int32
-              gl.get_integer_v(LibGL::GetPName::{{pname.id}}, pointerof(%value))
+              gl.get_integer_v(%pname, pointerof(%value))
               %value
 
             {% elsif type <= Int64 %}
               %value = uninitialized Int64
-              gl.get_integer_64v(LibGL::GetPName::{{pname.id}}, pointerof(%value))
+              gl.get_integer_64v(%pname, pointerof(%value))
               %value
 
             {% elsif type <= Float32 %}
               %value = uninitialized Float32
-              gl.get_float_v(LibGL::GetPName::{{pname.id}}, pointerof(%value))
+              gl.get_float_v(%pname, pointerof(%value))
               %value
 
             {% elsif type <= Float64 %}
               %value = uninitialized Float64
-              gl.get_float_64v(LibGL::GetPName::{{pname.id}}, pointerof(%value))
+              gl.get_float_64v(%pname, pointerof(%value))
               %value
 
             {% elsif type <= Bool %}
               %value = uninitialized Int32
-              gl.get_boolean_v(LibGL::GetPName::{{pname.id}}, pointerof(%value))
+              gl.get_boolean_v(%pname, pointerof(%value))
               !%value.zero?
 
             {% elsif type <= String %}
-              %value = gl.get_string(LibGL::StringName::{{pname.id}})
+              %value = gl.get_string(%pname)
               String.new(%value)
 
             {% else %}
               %value = uninitialized Int32
-              gl.get_integer_v(LibGL::GetPName::{{pname.id}}, pointerof(%value))
+              gl.get_integer_v(%pname, pointerof(%value))
               {{type}}.new(%value)
 
             {% end %}
@@ -83,8 +99,14 @@ module Gloop
 
       {% else %}
         def {{name.id}}
+          %pname = {% if pname.id.includes? "::" %}
+            LibGL::GetPName.new({{pname.id}}.to_u32!)
+          {% else %}
+            LibGL::GetPName::{{pname.id}}
+          {% end %}
+
           %value = uninitialized Int32
-          gl.get_integer_v(LibGL::GetPName::{{pname.id}}, pointerof(%value))
+          gl.get_integer_v(%pname, pointerof(%value))
 
           {% if block %}
             {{block.args.splat}} = %value
@@ -99,6 +121,8 @@ module Gloop
     # Defines a boolean getter method that retrieves an OpenGL parameter.
     # The *pname* is the OpenGL parameter name to retrieve.
     # This should be an enum value (just the name) from `LibGL::GetPName`.
+    # If the enum is not in `LibGL::GetPName` (for instance, a constant under LibGL),
+    # specify the full path as such: `LibGL::NUM_SHADING_LANGUAGE_VERSIONS`.
     # The *name* will be the name of the generated method, with a question mark appended to it.
     #
     # ```
@@ -106,8 +130,14 @@ module Gloop
     # ```
     private macro parameter?(pname, name)
       def {{name.id}}? : Bool
+        pname = {% if pname.id.includes? "::" %}
+          LibGL::GetPName.new({{pname.id}}.to_u32!)
+        {% else %}
+          LibGL::GetPName::{{pname.id}}
+        {% end %}
+
         value = uninitialized Int32
-        gl_call get_boolean_v(LibGL::GetPName::{{pname.id}}, pointerof(value))
+        gl_call get_boolean_v(pname, pointerof(value))
         !value.zero?
       end
     end
