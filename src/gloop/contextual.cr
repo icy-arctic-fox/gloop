c1d26cabcaddaea3f4f513235cff77f0f39eb52e
Michael Miller <icy.arctic.fox@gmail.com>
2021-09-25 18:51:51 -0600

    Add macro providing context-mapped storage

--- src/gloop/contextual.cr
+++ src/gloop/contextual.cr
@@ -14,5 +14,39 @@ module Gloop
     private def gl
       @context.gl
     end
+
+    # Defines getters and setters for a value associated with the context.
+    # The value lives outside of OpenGL, under Gloop's control.
+    #
+    # Must be used as follows:
+    # ```
+    # context_storage something : Thing
+    # ```
+    #
+    # Creates the following methods:
+    # ```
+    # name? : Type?         # Nillable getter that returns nil if not previously set.
+    # name : Type           # Getter that raises if not previously set.
+    # name = (value : Type) # Setter.
+    # ```
+    private macro context_storage(declaration)
+      {% type = declaration.type.resolve %}
+      {% name = declaration.var.id %}
+
+      @@%mutex = Mutex.new
+      @@%storage = {} of Context => {{type}}
+
+      def {{name}}? : {{type}}?
+        @@%mutex.synchronize { @@%storage[@context]? }
+      end
+
+      def {{name}} : {{type}}
+        @@%mutex.synchronize { @@%storage[@context] }
+      end
+
+      def {{name}}=(value : {{type}})
+        @@%mutex.synchronize { @@%storage[@context] = value }
+      end
+    end
   end
 end
