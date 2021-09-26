209ddfaf32a89134966790103510de397632f399
Michael Miller <icy.arctic.fox@gmail.com>
2021-09-25 19:01:24 -0600

    Add debug message type

---
+++ spec/gloop/debug/message_spec.cr
@@ -0,0 +1,55 @@
+require "../../spec_helper"
+
+Spectator.describe Gloop::Debug::Message do
+  subject(message) do
+    described_class.new(:application, :other, 12345, :notification, "Test message")
+  end
+  let(debug) { Gloop::Debug.new(context) }
+
+  describe "#initialize" do
+    it "sets the attributes" do
+      is_expected.to have_attributes(
+        source: Gloop::Debug::Source::Application,
+        type: Gloop::Debug::Type::Other,
+        id: 12345,
+        severity: Gloop::Debug::Severity::Notification,
+        message: "Test message"
+      )
+    end
+  end
+
+  describe "#insert" do
+    before_each { debug.clear }
+    after_each { debug.clear_on_message }
+
+    it "logs the message" do
+      received = nil.as(Gloop::Debug::Message?)
+      debug.on_message { |message| received = message }
+
+      message.insert(context)
+      expect(received).to_not be_nil, "Did not receive debug message"
+
+      expect(received.not_nil!).to have_attributes(
+        source: Gloop::Debug::Source::Application,
+        type: Gloop::Debug::Type::Other,
+        id: 12345,
+        severity: Gloop::Debug::Severity::Notification,
+        message: "Test message"
+      )
+    end
+  end
+
+  describe "#to_s" do
+    subject { message.to_s }
+
+    it "contains the attributes" do
+      aggregate_failures "attributes" do
+        is_expected.to contain("Application"), "Missing source"
+        is_expected.to contain("Other"), "Missing type"
+        is_expected.to contain("12345"), "Missing ID"
+        is_expected.to contain("Notification"), "Missing severity"
+        is_expected.to contain("Test message"), "Missing message"
+      end
+    end
+  end
+end
---
+++ src/gloop/debug/message.cr
@@ -0,0 +1,60 @@
+require "./severity"
+require "./source"
+require "./type"
+
+module Gloop
+  struct Debug
+    # Debug information received from OpenGL.
+    struct Message
+      # Source that produced the message.
+      getter source : Source
+
+      # Type of message.
+      getter type : Type
+
+      # Message ID.
+      getter id : UInt32
+
+      # Importance of the message.
+      getter severity : Severity
+
+      # Text in the message.
+      getter message : String
+
+      # Creates a new message intended to be sent to the OpenGL debug message queue.
+      # The *source* indicates where the message came from.
+      # It should be `Source::Application` or `Source::ThirdParty`.
+      def initialize(@source : Source, @type : Type, @id : UInt32, @severity : Severity, @message : String)
+      end
+
+      # Builds a message with the raw data received from OpenGL.
+      protected def initialize(source, type, @id, severity, length, string)
+        @source = Source.from_value(source.to_u32)
+        @type = Type.from_value(type.to_u32)
+        @severity = Severity.from_value(severity.to_u32)
+        @message = String.new(string, length)
+      end
+
+      # Inserts this debug message into the specified context.
+      #
+      # - OpenGL function: `glDebugMessageInsert`
+      # - OpenGL version: 4.3
+      @[GLFunction("glDebugMessageInsert", version: "4.3")]
+      def insert(context)
+        context.gl.debug_message_insert(
+          @source.to_unsafe,
+          @type.to_unsafe,
+          @id,
+          @severity.to_unsafe,
+          @message.bytesize,
+          @message.to_unsafe
+        )
+      end
+
+      # Produces a log-like string from the contents of the debug message.
+      def to_s(io)
+        io.printf("%7s [%s:%s] (%d) %s", severity, source, type, id, message)
+      end
+    end
+  end
+end
