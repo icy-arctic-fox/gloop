ab8d93236f7b6bbd6539c30173a190208f3df006
Michael Miller <icy.arctic.fox@gmail.com>
2021-09-29 10:31:11 -0600

    Clean up and test Buffers

---
+++ spec/gloop/buffers_spec.cr
@@ -0,0 +1,151 @@
+require "../spec_helper"
+
+Spectator.describe Gloop::Buffers do
+  subject(buffers) { Gloop::Buffers.new(context) }
+
+  describe "#array" do
+    subject { buffers.array }
+
+    it "retrieves a binding target for array buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::Array)
+    end
+  end
+
+  describe "#element_array" do
+    subject { buffers.element_array }
+
+    it "retrieves a binding target for element array buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::ElementArray)
+    end
+  end
+
+  describe "#pixel_pack" do
+    subject { buffers.pixel_pack }
+
+    it "retrieves a binding target for pixel pack buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::PixelPack)
+    end
+  end
+
+  describe "#pixel_unpack" do
+    subject { buffers.pixel_unpack }
+
+    it "retrieves a binding target for pixel unpack buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::PixelUnpack)
+    end
+  end
+
+  describe "#transform_feedback" do
+    subject { buffers.transform_feedback }
+
+    it "retrieves a binding target for transform feedback buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::TransformFeedback)
+    end
+  end
+
+  describe "#texture" do
+    subject { buffers.texture }
+
+    it "retrieves a binding target for texture buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::Texture)
+    end
+  end
+
+  describe "#copy_read" do
+    subject { buffers.copy_read }
+
+    it "retrieves a binding target for read buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::CopyRead)
+    end
+  end
+
+  describe "#copy_write" do
+    subject { buffers.copy_write }
+
+    it "retrieves a binding target for write buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::CopyWrite)
+    end
+  end
+
+  describe "#uniform" do
+    subject { buffers.uniform }
+
+    it "retrieves a binding target for uniform buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::Uniform)
+    end
+  end
+
+  describe "#draw_indirect" do
+    subject { buffers.draw_indirect }
+
+    it "retrieves a binding target for indirect draw buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::DrawIndirect)
+    end
+  end
+
+  describe "#atomic_counter" do
+    subject { buffers.atomic_counter }
+
+    it "retrieves a binding target for atomic counter buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::AtomicCounter)
+    end
+  end
+
+  describe "#dispatch_indirect" do
+    subject { buffers.dispatch_indirect }
+
+    it "retrieves a binding target for indirect dispatch buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::DispatchIndirect)
+    end
+  end
+
+  describe "#shader_storage" do
+    subject { buffers.shader_storage }
+
+    it "retrieves a binding target for shader storage buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::ShaderStorage)
+    end
+  end
+
+  describe "#query" do
+    subject { buffers.query }
+
+    it "retrieves a binding target for query buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::Query)
+    end
+  end
+
+  describe "#parameter" do
+    subject { buffers.parameter }
+
+    it "retrieves a binding target for parameter buffers" do
+      expect(&.target).to eq(Gloop::Buffer::Target::Parameter)
+    end
+  end
+
+  describe "#[]" do
+    it "returns a binding target for the specified target" do
+      binding = buffers[:array]
+      expect(binding.target).to eq(Gloop::Buffer::Target::Array)
+    end
+  end
+
+  describe "#copy" do
+    let(buffer_a) { Gloop::Buffer.generate(context) }
+    let(buffer_b) { Gloop::Buffer.generate(context) }
+    let(target_a) { buffers.copy_read }
+    let(target_b) { buffers.copy_write }
+
+    before_each do
+      target_a.bind(buffer_a)
+      target_b.bind(buffer_b)
+      target_a.data = Bytes[10, 11, 12, 13, 14, 15, 16, 17]
+      target_b.data = Bytes[20, 21, 22, 23, 24, 25, 26, 27]
+    end
+
+    it "copies data from one buffer to another" do
+      buffers.copy(:copy_read, :copy_write, 1, 2, 4)
+      expect(target_b.data).to eq(Bytes[20, 21, 11, 12, 13, 14, 26, 27])
+    end
+  end
+end
--- src/gloop/buffers.cr
+++ src/gloop/buffers.cr
@@ -1,60 +1,90 @@
 require "./buffer/bind_target"
 require "./buffer/target"
 require "./contextual"
+require "./size"
 
 module Gloop
   struct Buffers
     include Contextual
 
-    private macro buffer_target(name)
-      def {{name.id}}
-        Buffer::BindTarget.new(@context, {{name.id.symbolize}})
+    # Defines a method that returns a buffer binding target for the specified target.
+    #
+    # The *target* should be a symbol that refers to an enum value in `Buffer::Target`.
+    private macro buffer_target(target)
+      def {{target.id}} : Buffer::BindTarget
+        Buffer::BindTarget.new(@context, {{target.id.symbolize}})
       end
     end
 
+    # Retrieves a binding target for array buffers.
     buffer_target :array
 
+    # Retrieves a binding target for element array buffers.
     buffer_target :element_array
 
+    # Retrieves a binding target for pixel pack buffers.
     buffer_target :pixel_pack
 
+    # Retrieves a binding target for pixel unpack buffers.
     buffer_target :pixel_unpack
 
+    # Retrieves a binding target for transform feedback buffers.
     buffer_target :transform_feedback
 
+    # Retrieves a binding target for texture buffers.
     buffer_target :texture
 
+    # Retrieves a binding target for read buffers.
     buffer_target :copy_read
 
+    # Retrieves a binding target for write buffers.
     buffer_target :copy_write
 
+    # Retrieves a binding target for uniform buffers.
     buffer_target :uniform
 
+    # Retrieves a binding target for indirect draw buffers.
     buffer_target :draw_indirect
 
+    # Retrieves a binding target for atomic counter buffers.
     buffer_target :atomic_counter
 
+    # Retrieves a binding target for indirect dispatch buffers.
     buffer_target :dispatch_indirect
 
+    # Retrieves a binding target for shader storage buffers.
     buffer_target :shader_storage
 
+    # Retrieves a binding target for query buffers.
     buffer_target :query
 
+    # Retrieves a binding target for parameter buffers.
     buffer_target :parameter
 
-    def [](target : Buffer::Target)
+    # Retrieves the specified buffer binding target.
+    def [](target : Buffer::Target) : Buffer::BindTarget
       Buffer::BindTarget.new(@context, target)
     end
 
     # Copies a subset of data from a buffer bound to one target into one bound by another target.
+    #
+    # The *read_offset* indicates the byte offset to start copying from *read_target*.
+    # The *write_offset* indicates the byte offset to start copying into *write_target*.
+    # The *size* is the number of bytes to copy.
+    #
+    # - OpenGL function: `glCopyBufferSubData`
+    # - OpenGL version: 3.1
+    @[GLFunction("glCopyBufferSubData", version: "3.1")]
     def copy(from read_target : Buffer::Target, to write_target : Buffer::Target,
-             read_offset : Int, write_offset : Int, size : Int)
-      Buffer::BindTarget.copy(read_target, write_target, read_offset, write_offset, size)
+             read_offset : Size, write_offset : Size, size : Size)
+      gl.copy_buffer_sub_data(
+        read_target.copy_buffer_target, write_target.copy_buffer_target, read_offset, write_offset, size)
     end
   end
 
   struct Context
-    def buffers
+    # Retrieves an interface for the buffer binding targets for this context.
+    def buffers : Buffers
       Buffers.new(self)
     end
   end
