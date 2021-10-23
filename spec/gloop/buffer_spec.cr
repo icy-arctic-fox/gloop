7fe9bd3abdabdceab48879364e194c93093d8250
Michael Miller <icy.arctic.fox@gmail.com>
2021-10-23 13:21:00 -0600

    Move list type specs into corresponding base type

--- spec/gloop/buffer_spec.cr
+++ spec/gloop/buffer_spec.cr
@@ -804,3 +804,28 @@ Spectator.describe Gloop::Context do
     end
   end
 end
+
+Spectator.describe Gloop::BufferList do
+  subject(list) { Gloop::Buffer.create(context, 3) }
+
+  it "holds buffers" do
+    is_expected.to be_an(Indexable(Gloop::Buffer))
+    expect(&.size).to eq(3)
+    aggregate_failures "buffers" do
+      expect(list[0]).to be_a(Gloop::Buffer)
+      expect(list[1]).to be_a(Gloop::Buffer)
+      expect(list[2]).to be_a(Gloop::Buffer)
+    end
+  end
+
+  describe "#delete" do
+    it "deletes all buffers" do
+      list.delete
+      aggregate_failures "buffers" do
+        list.each do |buffer|
+          expect(buffer.exists?).to be_false
+        end
+      end
+    end
+  end
+end
--- spec/gloop/vertex_array_spec.cr
+++ spec/gloop/vertex_array_spec.cr
@@ -311,3 +311,28 @@ Spectator.describe Gloop::Context do
     end
   end
 end
+
+Spectator.describe Gloop::VertexArrayList do
+  subject(list) { Gloop::VertexArray.create(context, 3) }
+
+  it "holds vertex arrays" do
+    is_expected.to be_an(Indexable(Gloop::VertexArray))
+    expect(&.size).to eq(3)
+    aggregate_failures "vertex arrays" do
+      expect(list[0]).to be_a(Gloop::VertexArray)
+      expect(list[1]).to be_a(Gloop::VertexArray)
+      expect(list[2]).to be_a(Gloop::VertexArray)
+    end
+  end
+
+  describe "#delete" do
+    it "deletes all vertex arrays" do
+      list.delete
+      aggregate_failures "vertex arrays" do
+        list.each do |vertex_array|
+          expect(vertex_array.exists?).to be_false
+        end
+      end
+    end
+  end
+end
