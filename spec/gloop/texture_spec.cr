c68fdb350ec5e46ff8c6898765016cc40ae34f3b
Michael Miller <icy.arctic.fox@gmail.com>
2021-11-24 00:04:35 -0700

    Initial code for Texture

---
+++ spec/gloop/texture_spec.cr
@@ -0,0 +1,148 @@
+require "../spec_helper"
+
+Spectator.describe Gloop::Texture do
+  subject(texture) { described_class.create(context, :_2d) }
+
+  describe ".create" do
+    it "creates a texture" do
+      texture = described_class.create(context, :texture_2d)
+      expect(texture.exists?).to be_true
+    end
+
+    it "creates multiple textures" do
+      textures = described_class.create(context, :texture_2d, 3)
+      aggregate_failures do
+        expect(textures[0].exists?).to be_true
+        expect(textures[1].exists?).to be_true
+        expect(textures[2].exists?).to be_true
+      end
+    end
+  end
+
+  describe ".generate" do
+    pending "creates a textures", pending: "Texture#bind not implemented" do
+      textures = described_class.generate(context)
+      textures.bind
+      expect(vao.exists?).to be_true
+    end
+
+    pending "creates multiple textures", pending: "Texture#bind not implemented" do
+      textures = described_class.generate(context, 3)
+      aggregate_failures do
+        3.times do |i|
+          textures[i].bind
+          expect(textures[i].exists?).to be_true
+        end
+      end
+    end
+  end
+
+  describe ".none" do
+    subject { described_class.none(context) }
+
+    it "is a null object" do
+      expect(&.none?).to be_true
+    end
+  end
+
+  describe ".delete" do
+    it "deletes textures" do
+      textures = described_class.create(context, :texture_2d, 3)
+      described_class.delete(textures)
+      aggregate_failures do
+        expect(textures[0].exists?).to be_false
+        expect(textures[1].exists?).to be_false
+        expect(textures[2].exists?).to be_false
+      end
+    end
+  end
+
+  describe "#delete" do
+    it "deletes the texture" do
+      expect { texture.delete }.to change(&.exists?).from(true).to(false)
+    end
+  end
+  
+  context "Labelable" do
+    it "can be labeled" do
+      subject.label = "Test label"
+      expect(&.label).to eq("Test label")
+    end
+  end
+end
+
+Spectator.describe Gloop::Context do
+  subject { context }
+  let(texture) { context.create_texture }
+
+  describe "#create_texture" do
+    it "creates a texture" do
+      texture = context.create_texture(:texture_2d)
+      expect(texture.exists?).to be_true
+    ensure
+      texture.try(&.delete)
+    end
+  end
+
+  describe "#create_textures" do
+    it "creates multiple textures" do
+      textures = context.create_textures(:texture_2d, 3)
+      aggregate_failures do
+        expect(textures[0].exists?).to be_true
+        expect(textures[1].exists?).to be_true
+        expect(textures[2].exists?).to be_true
+      end
+    ensure
+      textures.delete if textures
+    end
+  end
+
+  describe "#generate_texture" do
+    pending "creates a texture", pending: "Texture#bind is not implemented" do
+    texture = context.generate_texture
+      texture.bind
+      expect(texture.exists?).to be_true
+    ensure
+      texture.try(&.delete)
+    end
+  end
+
+  describe "#generate_textures" do
+    pending "creates multiple textures", pending: "Texture#bind is not implemented" do
+    textures = context.generate_textures(3)
+      aggregate_failures do
+        3.times do |i|
+          textures[i].bind
+          expect(textures[i].exists?).to be_true
+        end
+      end
+    ensure
+      textures.delete if textures
+    end
+  end
+end
+
+Spectator.describe Gloop::TextureList do
+  subject(list) { Gloop::Texture.create(context, :texture_2d, 3) }
+
+  it "holds textures" do
+    is_expected.to be_an(Indexable(Gloop::Texture))
+    expect(&.size).to eq(3)
+    aggregate_failures "textures" do
+      expect(list[0]).to be_a(Gloop::Texture)
+      expect(list[1]).to be_a(Gloop::Texture)
+      expect(list[2]).to be_a(Gloop::Texture)
+    end
+  end
+
+  describe "#delete" do
+    it "deletes all textures" do
+      list.delete
+      aggregate_failures "textures" do
+        list.each do |texture|
+          expect(texture.exists?).to be_false
+        end
+      end
+    end
+  end
+end
---
+++ src/gloop/texture.cr
@@ -0,0 +1,206 @@
+require "./object"
+
+module Gloop
+  # Collection of one or more images.
+  #
+  # See: https://www.khronos.org/opengl/wiki/Texture
+  struct Texture < Object
+    # Types of textures.
+    enum Type : UInt32
+      Texture1D          = LibGL::TextureTarget::Texture1D
+      Texture2D          = LibGL::TextureTarget::Texture2D
+      Texture3D          = LibGL::TextureTarget::Texture3D
+      Texture1DArray     = LibGL::TextureTarget::Texture1DArray
+      Texture2DArray     = LibGL::TextureTarget::Texture2DArray
+      Rectangle          = LibGL::TextureTarget::TextureRectangle
+      CubeMap            = LibGL::TextureTarget::TextureCubeMap
+      CubeMapArray       = LibGL::TextureTarget::TextureCubeMapArray
+      Buffer             = LibGL::TextureTarget::TextureBuffer
+      MultiSample2D      = LibGL::TextureTarget::Texture2DMultisample
+      MultiSample2DArray = LibGL::TextureTarget::Texture2DMultisampleArray
+
+      # Creates a texture type from a symbol.
+      #
+      # This is intended to be used as a workaround for Crystal's limitations and auto-generated names.
+      def self.new(value : Symbol)
+        case value
+        when :_1d, :"1d", :texture_1d                   then Texture1D
+        when :_2d, :"2d", :texture_2d                   then Texture2D
+        when :_3d, :"3d", :texture_3d                   then Texture3D
+        when :_1d_array, :"1d_array", :texture_1d_array then Texture1DArray
+        when :_2d_array, :"2d_array", :texture_2d_array then Texture2DArray
+        else                                                 raise ArgumentError.new("Invalid texture type")
+        end
+      end
+
+      # Converts to an OpenGL enum.
+      def to_unsafe
+        LibGL::TextureTarget.new(value)
+      end
+    end
+
+    # Generates a new texture.
+    #
+    # The texture isn't assigned a type or resources until it is bound.
+    #
+    # See: `#create`
+    #
+    # - OpenGL function: `glGenTextures`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGenTextures", version: "2.0")]
+    def self.generate(context) : self
+      name = uninitialized Name
+      context.gl.gen_textures(1, pointerof(name))
+      new(context, name)
+    end
+
+    # Generates multiple textures.
+    #
+    # The textures aren't assigned a type or resources until they are bound.
+    #
+    # See: `#create`
+    #
+    # - OpenGL function: `glGenTextures`
+    # - OpenGL version: 2.0
+    @[GLFunction("glGenTextures", version: "2.0")]
+    def self.generate(context, count : Int) : TextureList
+      TextureList.new(context, count) do |names|
+        context.gl.gen_textures(count, names)
+      end
+    end
+
+    # Creates a new texture of the specified type.
+    #
+    # - OpenGL function: `glCreateTextures`
+    # - OpenGL version: 4.5
+    @[GLFunction("glCreateTextures", version: "4.5")]
+    def self.create(context, type : Type) : self
+      name = uninitialized Name
+      context.gl.create_textures(type.to_unsafe, 1, pointerof(name))
+      new(context, name)
+    end
+
+    # Creates a new texture of the specified type.
+    #
+    # - OpenGL function: `glCreateTextures`
+    # - OpenGL version: 4.5
+    @[GLFunction("glCreateTextures", version: "4.5")]
+    @[AlwaysInline]
+    def self.create(context, type : Symbol) : self
+      create(context, Type.new(type))
+    end
+
+    # Creates multiple textures of the specified type.
+    #
+    # - OpenGL function: `glCreateTextures`
+    # - OpenGL version: 4.5
+    @[GLFunction("glCreateTextures", version: "4.5")]
+    def self.create(context, type : Type, count : Int) : TextureList
+      TextureList.new(context, count) do |names|
+        context.gl.create_textures(type.to_unsafe, count, names)
+      end
+    end
+
+    # Creates multiple textures of the specified type.
+    #
+    # - OpenGL function: `glCreateTextures`
+    # - OpenGL version: 4.5
+    @[GLFunction("glCreateTextures", version: "4.5")]
+    @[AlwaysInline]
+    def self.create(context, type : Symbol, count : Int) : TextureList
+      create(context, Type.new(type), count)
+    end
+
+    # Deletes this texture.
+    #
+    # - OpenGL function: `glDeleteTextures`
+    # - OpenGL version: 2.0
+    @[GLFunction("glDeleteTextures", version: "2.0")]
+    def delete
+      gl.delete_textures(1, pointerof(@name))
+    end
+
+    # Deletes multiple textures.
+    #
+    # - OpenGL function: `glDeleteTextures`
+    # - OpenGL version: 2.0
+    @[GLFunction("glDeleteTextures", version: "2.0")]
+    def self.delete(textures : Enumerable(self)) : Nil
+      textures.group_by(&.context).each do |context, subset|
+        names = subset.map(&.to_unsafe)
+        context.gl.delete_textures(names.size, names.to_unsafe)
+      end
+    end
+
+    # Checks if the texture is known by OpenGL.
+    #
+    # - OpenGL function: `glIsTexture`
+    # - OpenGL version: 2.0
+    @[GLFunction("glIsTexture", version: "2.0")]
+    def exists?
+      value = gl.is_texture(to_unsafe)
+      !value.false?
+    end
+
+    # Indicates that this is a texture object.
+    def object_type
+      Object::Type::Texture
+    end
+  end
+
+  struct Context
+    # Generates a new texture.
+    #
+    # See: `Texture.generate`
+    def generate_texture : Texture
+      Texture.generate(self)
+    end
+
+    # Generates multiple textures.
+    #
+    # See: `Texture.generate`
+    def generate_textures(count : Int) : TextureList
+      Texture.generate(self, count)
+    end
+
+    # Creates a new texture of the specified type.
+    #
+    # See: `Texture.create`
+    def create_texture(type : Texture::Type) : Texture
+      Texture.create(self, type)
+    end
+
+    # Creates a new texture of the specified type.
+    #
+    # See: `Texture.create`
+    def create_texture(type : Symbol) : Texture
+      Texture.create(self, type)
+    end
+
+    # Creates multiple textures of the specified type.
+    #
+    # See: `Texture.create`
+    def create_textures(type : Texture::Type, count : Int) : TextureList
+      Texture.create(self, type, count)
+    end
+
+    # Creates multiple textures of the specified type.
+    #
+    # See: `Texture.create`
+    def create_textures(type : Symbol, count : Int) : TextureList
+      Texture.create(self, type, count)
+    end
+  end
+
+  # Collection of textures belonging to the same context.
+  struct TextureList < ObjectList(Texture)
+    # Deletes all textures in the list.
+    #
+    # - OpenGL function: `glDeleteTextures`
+    # - OpenGL version: 2.0
+    @[GLFunction("glDeleteTextures", version: "2.0")]
+    def delete : Nil
+      gl.delete_textures(size, to_unsafe)
+    end
+  end
+end
