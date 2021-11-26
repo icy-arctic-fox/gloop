af7692fac3b41099f54d5cce58a0e8b145a9378d
Michael Miller <icy.arctic.fox@gmail.com>
2021-11-25 20:16:06 -0700

    Add other symbols to type initializer

--- src/gloop/texture.cr
+++ src/gloop/texture.cr
@@ -29,6 +29,12 @@ module Gloop
         when :_3d, :"3d", :texture_3d                   then Texture3D
         when :_1d_array, :"1d_array", :texture_1d_array then Texture1DArray
         when :_2d_array, :"2d_array", :texture_2d_array then Texture2DArray
+        when :rectangle                                 then Rectangle
+        when :cube_map                                  then CubeMap
+        when :cube_map_array                            then CubeMapArray
+        when :buffer                                    then Buffer
+        when :multi_sample_2d                           then MultiSample2D
+        when :multi_sample_2d_array                     then MultiSample2DArray
         else                                                 raise ArgumentError.new("Invalid texture type")
         end
       end
